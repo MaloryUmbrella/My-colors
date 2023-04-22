eff014c18c2fd300c805829292d1ff0fe219773b
Hugo Bossard <hugo.bossard@etudiant.univ-lr.fr>
2023-04-22 16:17:07 +0200

    Initial commit

---
+++ src/components/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <>
+            <div>
+                <p> Paragraphe </p>
+            </div>
+        </>
+  );
+};
+
+export default Main;
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Main from './components/main'
+import "./index.css"
+
+const JSXRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+        <Route path='/' element={Main()}> </Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <RouterProvider router={JSXRouter} />
+);
