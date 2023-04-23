08ab9bbdaa75a245064d3eeecd95b0551fcf9c1a
Malory8222 <claraa8222@gmail.com>
2023-04-23 18:13:08 +0200

    Add rbgToHex converter on onClick onto a color to copy it unto clipboard

--- src/components/atomes/div_color.js
+++ src/components/atomes/div_color.js
@@ -27,11 +27,38 @@ const DivColor = ( props ) => {
         }
     }
 
+    let rgbToHex = (r, g, b, withoutHash) => {
+        let hex_color;
+
+        if (withoutHash) {
+            hex_color = "" + r.toString(16) + g.toString(16) + b.toString(16);
+        }
+        else {
+            hex_color = "#" + r.toString(16) + g.toString(16) + b.toString(16);
+        }
+
+        return hex_color;
+    }
+
+    let copyColor = (div) => {
+        let divColor = document.getElementById(`div${div}`);
+
+        let color = divColor.style.getPropertyValue("background-color");
+
+        let color_to_hex = rgbToHex(parseInt(color.substring(4,color.indexOf(","))), parseInt(color.substring(color.indexOf(",") + 2, color.lastIndexOf(","))), parseInt(color.substring(color.lastIndexOf(",") + 2, color.length - 1)), true);
+
+        navigator.clipboard.writeText(color_to_hex).then(function() {
+                alert("La couleur a bien été copiée dans le presse papier");
+            }, function() {
+                alert("La couleur n'a pas pu être copiée dans le presse papier");
+        });
+    }
+
     let key = parseInt(String(props.id));
 
     return (
         <div className="col-md-15 color_wrapper" key={key}>
-			<div className="color" style={{ backgroundColor: props.color }} id={`div${props.id}`} key={key + 1} data-color={props.color}>
+			<div className="color" style={{ backgroundColor: props.color }} id={`div${props.id}`} key={key + 1} data-color={props.color} onClick={() => copyColor(props.id)}>
                 <div className="swipe"> </div>
             </div>
 			<div className="color_tools">
